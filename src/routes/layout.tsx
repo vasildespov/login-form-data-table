d4abb815157b6ac7414b5a784c2041933bea623f
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-11-11 21:42:56 +0200

    refactor: move render condition outside header

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,17 @@
 import { Header } from "@/components/header";
 import { NoConnection } from "@/components/no-connection";
 import { useConnection } from "@/hooks/useConnection";
+import { useLocation } from "react-router";
 import { Outlet } from "react-router";
 
 export const RootLayout = () => {
   const isOnline = useConnection();
+  const location = useLocation();
 
   return (
     <div className="relative flex min-h-screen flex-col">
       {!isOnline && <NoConnection />}
-      <Header />
+      {location.pathname !== "/" && <Header />}
       <main className="mt-15 flex flex-1 items-center justify-center p-5">
         <Outlet />
       </main>

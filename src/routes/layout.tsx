b285142cda1342494afcae38bc1a611b421b389f
Vasil Despov <60199602+vasildespov@users.noreply.github.com>
2025-11-12 13:33:48 +0200

    feat: add global layout loader indicator

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,19 +1,26 @@
 import { Header } from "@/components/header";
 import { NoConnection } from "@/components/no-connection";
 import { useConnection } from "@/hooks/useConnection";
-import { useLocation } from "react-router";
+import { LoaderCircle } from "lucide-react";
+import { useLocation, useNavigation } from "react-router";
 import { Outlet } from "react-router";
 
 export const RootLayout = () => {
   const isOnline = useConnection();
   const location = useLocation();
+  const navigation = useNavigation();
+
+  const isNavigating =
+    navigation.state === "loading" &&
+    navigation.location &&
+    navigation.location.pathname !== location.pathname;
 
   return (
     <div className="relative flex min-h-screen flex-col">
       {!isOnline && <NoConnection />}
       {location.pathname !== "/" && <Header />}
       <main className="mt-15 flex flex-1 items-center justify-center p-5">
-        <Outlet />
+        {isNavigating ? <LoaderCircle className="animate-spin" /> : <Outlet />}
       </main>
     </div>
   );
